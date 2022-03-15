9ff2aa6a96e918e582398d28f395e2f0c9c3d89a
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-15 23:40:40 +0700

    Add hitbox and lag compensation scripts

---
+++ TestLagCompensation1/Assets/Scripts/HitBox.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class HitBox : MonoBehaviour
+{
+    [System.Serializable]
+    public struct TransformHistory
+    {
+        public long Time { get; set; }
+        public Vector3 Position { get; set; }
+        public Quaternion Rotation { get; set; }
+    }
+
+    [SerializeField]
+    protected float damageRate = 1f;
+
+    public Collider CacheCollider { get; private set; }
+    public Rigidbody CacheRigidbody { get; private set; }
+    public Collider2D CacheCollider2D { get; private set; }
+    public Rigidbody2D CacheRigidbody2D { get; private set; }
+    public int Index { get; private set; }
+
+    protected bool isSetup;
+    protected Vector3 defaultLocalPosition;
+    protected Quaternion defaultLocalRotation;
+    protected List<TransformHistory> histories = new List<TransformHistory>();
+
+#if UNITY_EDITOR
+    [Header("Rewind Debugging")]
+    public Color debugHistoryColor = new Color(0, 1, 0, 0.25f);
+    public Color debugRewindColor = new Color(0, 0, 1, 0.5f);
+    private Vector3? debugRewindPosition;
+    private Quaternion? debugRewindRotation;
+    private Vector3? debugRewindCenter;
+    private Vector3? debugRewindSize;
+#endif
+
+    private void Awake()
+    {
+        CacheCollider = GetComponent<Collider>();
+        if (CacheCollider)
+        {
+            CacheRigidbody = gameObject.GetComponent<Rigidbody>();
+            CacheRigidbody.useGravity = false;
+            CacheRigidbody.isKinematic = true;
+#if UNITY_EDITOR
+            debugRewindCenter = CacheCollider.bounds.center - transform.position;
+            debugRewindSize = CacheCollider.bounds.size;
+#endif
+            return;
+        }
+        CacheCollider2D = GetComponent<Collider2D>();
+        if (CacheCollider2D)
+        {
+            CacheRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+            CacheRigidbody2D.gravityScale = 0;
+            CacheRigidbody2D.isKinematic = true;
+#if UNITY_EDITOR
+            debugRewindCenter = CacheCollider2D.bounds.center - transform.position;
+            debugRewindSize = CacheCollider2D.bounds.size;
+#endif
+        }
+    }
+
+    public virtual void Setup(int index)
+    {
+        isSetup = true;
+        defaultLocalPosition = transform.localPosition;
+        defaultLocalRotation = transform.localRotation;
+        Index = index;
+    }
+
+#if UNITY_EDITOR
+    protected virtual void OnDrawGizmos()
+    {
+        if (debugRewindCenter.HasValue &&
+            debugRewindSize.HasValue)
+        {
+            Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+            foreach (TransformHistory history in histories)
+            {
+                Matrix4x4 transformMatrix = Matrix4x4.TRS(history.Position + debugRewindCenter.Value, history.Rotation, debugRewindSize.Value);
+                Gizmos.color = debugHistoryColor;
+                Gizmos.matrix = transformMatrix;
+                Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+            }
+            if (debugRewindPosition.HasValue &&
+                debugRewindRotation.HasValue)
+            {
+                Matrix4x4 transformMatrix = Matrix4x4.TRS(debugRewindPosition.Value + debugRewindCenter.Value, debugRewindRotation.Value, debugRewindSize.Value);
+                Gizmos.color = debugRewindColor;
+                Gizmos.matrix = transformMatrix;
+                Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            }
+            Gizmos.matrix = oldGizmosMatrix;
+        }
+        Handles.Label(transform.position, name + "(HitBox)");
+    }
+#endif
+
+    internal void Rewind(long currentTime, long rewindTime)
+    {
+        TransformHistory beforeRewind = default;
+        TransformHistory afterRewind = default;
+        for (int i = 0; i < histories.Count; ++i)
+        {
+            if (beforeRewind.Time > 0 && beforeRewind.Time <= rewindTime && histories[i].Time >= rewindTime)
+            {
+                afterRewind = histories[i];
+                break;
+            }
+            else
+            {
+                beforeRewind = histories[i];
+            }
+            if (histories.Count - 1 == i)
+            {
+                afterRewind = new TransformHistory()
+                {
+                    Position = transform.position,
+                    Rotation = transform.rotation,
+                    Time = currentTime,
+                };
+            }
+        }
+        long durationToRewindTime = rewindTime - beforeRewind.Time;
+        long durationBetweenRewindTime = afterRewind.Time - beforeRewind.Time;
+        float lerpProgress = (float)durationToRewindTime / (float)durationBetweenRewindTime;
+        transform.position = Vector3.Lerp(beforeRewind.Position, afterRewind.Position, lerpProgress);
+        transform.rotation = Quaternion.Slerp(beforeRewind.Rotation, afterRewind.Rotation, lerpProgress);
+#if UNITY_EDITOR
+        debugRewindPosition = transform.position;
+        debugRewindRotation = transform.rotation;
+#endif
+    }
+
+    internal void Restore()
+    {
+        transform.localPosition = defaultLocalPosition;
+        transform.localRotation = defaultLocalRotation;
+    }
+
+    public void AddTransformHistory(long time)
+    {
+        if (histories.Count == DefaultLagCompensationManager.Instance.MaxHistorySize)
+            histories.RemoveAt(0);
+        histories.Add(new TransformHistory()
+        {
+            Time = time,
+            Position = transform.position,
+            Rotation = transform.rotation,
+        });
+    }
+}
---
+++ TestLagCompensation1/Assets/Scripts/LagCompensationManager.cs
@@ -0,0 +1,116 @@
+using LiteNetLibManager;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultLagCompensationManager : MonoBehaviour
+{
+    private readonly Dictionary<uint, HitBox[]> HitBoxes = new Dictionary<uint, HitBox[]>();
+    public float snapShotInterval = 0.06f;
+    public int maxHistorySize = 16;
+    public int MaxHistorySize { get { return maxHistorySize; } }
+    public static DefaultLagCompensationManager Instance { get; private set; }
+    private readonly List<HitBox> hitBoxes = new List<HitBox>();
+    private float snapShotCountDown = 0f;
+
+    private LiteNetLibGameManager manager;
+
+    private void Awake()
+    {
+        Instance = this;
+        manager = FindObjectOfType<LiteNetLibGameManager>();
+    }
+
+    public bool AddHitBoxes(uint objectId, HitBox[] hitBoxes)
+    {
+        if (HitBoxes.ContainsKey(objectId))
+            return false;
+        HitBoxes.Add(objectId, hitBoxes);
+        return true;
+    }
+
+    public bool RemoveHitBoxes(uint objectId)
+    {
+        return HitBoxes.Remove(objectId);
+    }
+
+    public bool SimulateHitBoxes(long connectionId, long targetTime, Action action)
+    {
+        if (action == null || !BeginSimlateHitBoxes(connectionId, targetTime))
+            return false;
+        action.Invoke();
+        EndSimulateHitBoxes();
+        return true;
+    }
+
+    public bool SimulateHitBoxesByRtt(long connectionId, Action action)
+    {
+        if (action == null || !BeginSimlateHitBoxesByRtt(connectionId))
+            return false;
+        action.Invoke();
+        EndSimulateHitBoxes();
+        return true;
+    }
+
+    public bool BeginSimlateHitBoxes(long connectionId, long targetTime)
+    {
+        if (!manager.IsServer || !manager.ContainsPlayer(connectionId))
+            return false;
+        LiteNetLibPlayer player = manager.GetPlayer(connectionId);
+        return InternalBeginSimlateHitBoxes(player, targetTime);
+    }
+
+    public bool BeginSimlateHitBoxesByRtt(long connectionId)
+    {
+        if (!manager.IsServer || !manager.ContainsPlayer(connectionId))
+            return false;
+        LiteNetLibPlayer player = manager.GetPlayer(connectionId);
+        long targetTime = manager.ServerTimestamp - player.Rtt;
+        return InternalBeginSimlateHitBoxes(player, targetTime);
+    }
+
+    private bool InternalBeginSimlateHitBoxes(LiteNetLibPlayer player, long targetTime)
+    {
+        hitBoxes.Clear();
+        foreach (uint subscribingObjectId in player.GetSubscribingObjectIds())
+        {
+            if (HitBoxes.ContainsKey(subscribingObjectId))
+                hitBoxes.AddRange(HitBoxes[subscribingObjectId]);
+        }
+        long time = manager.ServerTimestamp;
+        for (int i = 0; i < hitBoxes.Count; ++i)
+        {
+            if (hitBoxes[i] != null)
+                hitBoxes[i].Rewind(time, targetTime);
+        }
+        return true;
+    }
+
+    public void EndSimulateHitBoxes()
+    {
+        for (int i = 0; i < hitBoxes.Count; ++i)
+        {
+            if (hitBoxes[i] != null)
+                hitBoxes[i].Restore();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!manager.IsServer)
+            return;
+        snapShotCountDown -= Time.fixedDeltaTime;
+        if (snapShotCountDown > 0)
+            return;
+        snapShotCountDown = snapShotInterval;
+        long time = manager.ServerTimestamp;
+        foreach (HitBox[] hitBoxesArray in HitBoxes.Values)
+        {
+            foreach (HitBox hitBox in hitBoxesArray)
+            {
+                hitBox.AddTransformHistory(time);
+            }
+        }
+    }
+}
